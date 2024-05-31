b3befa8b407a4497f8f159acfb9a645f37e77ad1
rayliu7717 <rayliu2004@gmail.com>
2024-05-30 21:37:14 -0400

    add another state member showCounter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,8 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+
   const incrementHandler = () => {
     dispatch({type:'increment'})
   };
@@ -15,12 +17,13 @@ const Counter = () => {
     dispatch({type:'increase', amount: 5})
   };
   const toggleCounterHandler = () => {
+    dispatch({type:'toggleCounter'})
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show &&<div className={classes.value}>{counter}</div>}
       <button onClick={incrementHandler}>Increment</button>
       <button onClick={decrementHandler}>Decrement</button>
       <button onClick={increaseHandler}>Increase by 5</button>
