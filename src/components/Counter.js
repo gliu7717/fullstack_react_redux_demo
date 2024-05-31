cf8330ea52edf97d8f043613a65ad3663c580d97
rayliu7717 <rayliu2004@gmail.com>
2024-05-30 21:11:14 -0400

    redux store and reducer

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import { useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const incrementHandler = () => {
+  };
+  const decrementHandler = () => {
+  };
+  const increaseHandler = () => {
+  };
+  const toggleCounterHandler = () => {
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
+      <button onClick={increaseHandler}>Increase by 5</button>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
